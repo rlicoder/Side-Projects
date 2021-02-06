8207caae6aaffa13acf631b0e015e85e3dff6a1c
rlicoder <rl.nguyen@outlook.com>
2021-02-05 17:09:30 -0800

    added file support and IP element

--- RasPi-Headless-IPSend/self-email.py
+++ RasPi-Headless-IPSend/self-email.py
@@ -1,14 +1,18 @@
 import smtplib
 import ssl
+from urllib.request import urlopen
 
 #dont name this file "email.py or else it will conflict with imported libraries"
 
+my_ip = urlopen('http://ip.42.pl/raw').read()
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 #need to enable "less secure app access" on your google account.
-email = input("Email: ");
-password = input("Type your password and press enter: ")
-message = "test"
+email_file = open("email.txt")
+password_file = open("password.txt")
+email = email_file.readline()
+password = password_file.readline()
+message = my_ip
 
 with smtplib.SMTP_SSL(smtp_server, port) as server:
     server.login(email, password)
