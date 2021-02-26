5417ef56f3db42963ef3cdf302a73f45e30054c1
rlicoder <rl.nguyen@outlook.com>
2021-02-25 22:06:55 -0800

    empty html check and better first step verif

--- GPU Scalp/gpuscalp.py
+++ GPU Scalp/gpuscalp.py
@@ -14,6 +14,9 @@ sleep(sleeptime)
 html = bot.page_source
 
 while (html.find('Waitlist is currently closed') != -1):
+    bot.refresh()
+    sleep(sleeptime)
+    html = bot.page_source
     while (html.find('Waitlist is currently closed') != -1):
         bot.refresh()
         sleep(sleeptime)
@@ -22,12 +25,14 @@ while (html.find('Waitlist is currently closed') != -1):
     bot.get('https://v2.waitwhile.com/welcome/microcentertust')
     bot.refresh()
     sleep(sleeptime+5)
+    if (len(html) == 0):
+        continue
     html = bot.page_source
 
 f.write(html)
 f.close()
 
-for i in range(0, 100):
+for i in range(0, 1000):
     print('\a')
     sleep(1)
 
