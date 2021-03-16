a7071f622e5cee75489a4825d224c9b0ef54b784
rlicoder <rl.nguyen@outlook.com>
2021-03-16 14:31:24 -0700

    yelp scalper

--- EDD_automator/scalp.py
+++ EDD_automator/scalp.py
@@ -1,16 +1,48 @@
 from bs4 import BeautifulSoup as bs
 import re
 import urllib.request
+import unicodedata
 
-yelp = urllib.request.urlopen('https://www.yelp.com/search?find_desc=&find_loc=Corona%2C+CA&ns=1')
 
-soup = bs(yelp, 'html.parser')
+cities = ['riverside']
 
-print(soup)
-table = soup.find_all('div')
+for j in cities:
+    for k in range(0, 200, 10):
+        url = 'https://www.yelp.com/search?find_desc=&find_loc='
+        url += j
+        url += '%2C%20CA&ns=1&start='
+        print(j + ' ' + str(k))
+        url += str(k)
 
-print(len(table))
-for i in table:
-    print(i.get_text())
+        yelp = urllib.request.urlopen(url)
+
+        soup = bs(yelp, 'html.parser')
+
+        table = soup.find_all('div', class_= re.compile('^scrollablePhotos'))
+
+        f = open ('places.txt', 'a')
+        #print(url)
+        for i in range(0, len(table)):
+            if i == 0:
+                continue
+            if (table[i] is None):
+                continue
+            raw_title = re.search('\D+?(?=\d)', table[i].get_text())
+            if raw_title is None:
+                continue
+            raw_title_text = unicodedata.normalize('NFKD', raw_title.group())
+            title = re.sub('(\d+?|)\. ', '', raw_title_text)
+            raw_phone = re.search('\(\d{3}\) \d{3}-\d{4}', table[i].get_text())
+            if raw_phone is None:
+                continue
+            phone = re.sub('\D', '', raw_phone.group())
+            if not title:
+                continue
+            if not phone:
+                continue
+            f.write(title)
+            f.write('\n')
+            f.write(phone)
+            f.write('\n')
 
 
