2fed948929282ebe914830fe4467a2c1d11928ae
rlicoder <rl.nguyen@outlook.com>
2021-05-29 19:23:30 -0700

    updates over MC

---
+++ Survey_Engine/build/Debug/GNU-MacOSX/Form.o.d
@@ -0,0 +1,8 @@
+build/Debug/GNU-MacOSX/Form.o: Form.cpp Form.hpp Question.hpp \
+  /usr/local/include/bits/stdc++.h
+
+Form.hpp:
+
+Question.hpp:
+
+/usr/local/include/bits/stdc++.h:
--- Time Logger/nbproject/Makefile-Debug.mk
+++ Time Logger/nbproject/Makefile-Debug.mk
@@ -21,8 +21,8 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=MinGW-Windows
-CND_DLIB_EXT=dll
+CND_PLATFORM=GNU-MacOSX
+CND_DLIB_EXT=dylib
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -62,9 +62,9 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/time_logger.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/time_logger
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/time_logger.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/time_logger: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/time_logger ${OBJECTFILES} ${LDLIBSOPTIONS}
 
