1c244629bfbef216dcfac63dc1d59ac39bcdf2b3
rlicoder <rl.nguyen@outlook.com>
2021-07-04 21:03:30 -0700

    directory update

--- Youtube-Playlist-Downloader/main.py
+++ Youtube-Playlist-Downloader/main.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import youtube_dl
 import os
 import re
+import sys
 
 link = input('Enter your link here: ')
 dirname = input('Enter the name of the folder you want to store these songs in: ')
@@ -10,8 +11,9 @@ os.mkdir(dirname)
 
 ydl_cmd_options = {
     'format': 'bestaudio/best',
-    #'playliststart': 185,
-    'cookiefile': 'cookies.txt',
+    'playliststart': 170,
+    #'cookiefile': 'cookies.txt',
+    'ignoreerrors': True,
     'postprocessors':
     [{
         'key': 'FFmpegExtractAudio',
@@ -22,12 +24,16 @@ ydl_cmd_options = {
     'outtmpl': dirname + '/%(title)s.%(ext)s',
 }
 
-
+print("downloading your video(s), please wait...")
+sys.stdout = open('debug.txt', 'w')
+sys.stderr = sys.stdout
 with youtube_dl.YoutubeDL(ydl_cmd_options) as ydl:
     ydl.download([link])
+sys.stdout = sys.__stdout__
+sys.stderr = sys.__stderr__
+print("done!")
 
 pat = '[\[\(].+?[\]\)]'
-to_remove = set()
 
 os.chdir(os.getcwd() + '/' + dirname)
 
